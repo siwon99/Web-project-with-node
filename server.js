e75c8f8e849425e359d976703af38f27f5696fb3
siwon99 <kimsiwon0707@gmail.com>
2023-12-10 21:31:52 +0900

    feat: 수정기능 만들기

--- server.js
+++ server.js
@@ -59,3 +59,20 @@ app.get('/detail/:id', async (요청, 응답) => {
     응답.status(400).send('이상한 url 입력금지')
   }
 })
+
+app.get('/edit/:id', async (요청, 응답) => {
+  let result = await db.collection('post').findOne({ _id : new ObjectId(요청.params.id)})
+  console.log(result)
+  응답.render('edit.ejs', {result : result})
+})
+
+app.post('/edit', async (요청, 응답)=>{
+  try {
+    await db.collection('post').updateOne({ _id : new ObjectId(요청.body.id) },
+      {$set : { title : 요청.body.title, content : 요청.body.content }
+    })
+    응답.redirect('/list')
+  } catch(e) {
+    응답.status(400).send('Error! 내용채우기')
+  }
+}) 
